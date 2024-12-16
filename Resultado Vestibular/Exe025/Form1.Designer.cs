65f1a5470c6e8b16848e258b0b73111ae0ebb2ae
gigidborges <giovanaborges1903@gmail.com>
2024-12-15 22:54:18 -0300

    Add files via upload

---
+++ Resultado Vestibular/Exe025/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Exe025
+{
+    partial class FrmExe025
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LblResultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtNumero = new System.Windows.Forms.TextBox();
+            this.BtnConsultar = new System.Windows.Forms.Button();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.BtnConsultar1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(499, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ESCOLA TÉCNICA ESTADUAL ETESP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(545, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CONSULTA RESULTADO VESTIBULINHO";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.LblResultado);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(30, 246);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(777, 98);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status:";
+            // 
+            // LblResultado
+            // 
+            this.LblResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.LblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResultado.Location = new System.Drawing.Point(30, 38);
+            this.LblResultado.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LblResultado.Name = "LblResultado";
+            this.LblResultado.Size = new System.Drawing.Size(732, 32);
+            this.LblResultado.TabIndex = 5;
+            this.LblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 176);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(348, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "NÚMERO DE INSCRIÇÃO:";
+            // 
+            // TxtNumero
+            // 
+            this.TxtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumero.Location = new System.Drawing.Point(364, 175);
+            this.TxtNumero.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtNumero.MaxLength = 6;
+            this.TxtNumero.Name = "TxtNumero";
+            this.TxtNumero.Size = new System.Drawing.Size(102, 35);
+            this.TxtNumero.TabIndex = 0;
+            this.TxtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BtnConsultar
+            // 
+            this.BtnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConsultar.Location = new System.Drawing.Point(650, 158);
+            this.BtnConsultar.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnConsultar.Name = "BtnConsultar";
+            this.BtnConsultar.Size = new System.Drawing.Size(157, 72);
+            this.BtnConsultar.TabIndex = 1;
+            this.BtnConsultar.Text = "Consultar II (IndexOf)";
+            this.BtnConsultar.UseVisualStyleBackColor = true;
+            this.BtnConsultar.Click += new System.EventHandler(this.BtnConsultar_Click);
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpar.Location = new System.Drawing.Point(30, 366);
+            this.BtnLimpar.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(178, 44);
+            this.BtnLimpar.TabIndex = 4;
+            this.BtnLimpar.Text = "&Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(614, 366);
+            this.BtnSair.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(178, 44);
+            this.BtnSair.TabIndex = 5;
+            this.BtnSair.Text = "&Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // BtnConsultar1
+            // 
+            this.BtnConsultar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConsultar1.Location = new System.Drawing.Point(477, 158);
+            this.BtnConsultar1.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnConsultar1.Name = "BtnConsultar1";
+            this.BtnConsultar1.Size = new System.Drawing.Size(169, 72);
+            this.BtnConsultar1.TabIndex = 6;
+            this.BtnConsultar1.Text = "Consultar I (Array)";
+            this.BtnConsultar1.UseVisualStyleBackColor = true;
+            this.BtnConsultar1.Click += new System.EventHandler(this.BtnConsultar1_Click);
+            // 
+            // FrmExe025
+            // 
+            this.AcceptButton = this.BtnConsultar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(821, 438);
+            this.Controls.Add(this.BtnConsultar1);
+            this.Controls.Add(this.BtnConsultar);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.TxtNumero);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "FrmExe025";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exe025";
+            this.Load += new System.EventHandler(this.FrmExe025_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label LblResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtNumero;
+        private System.Windows.Forms.Button BtnConsultar;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.Button BtnConsultar1;
+    }
+}
+
---
+++ Resultado Vestibular/Exe025/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Exe025
+{
+    public partial class FrmExe025 : Form
+    {
+        //Declaração do Array da lista de classificados
+        int[] listaClassificados = new int[10];
+
+
+        public FrmExe025()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            TxtNumero.Text = "";
+            LblResultado.Text = "";
+            TxtNumero.Focus();
+        }
+
+        private void FrmExe025_Load(object sender, EventArgs e)
+        {
+            //Conteúdo do Array - Lista das inscrições CLASSIFICADAS
+            listaClassificados[0] = 10514;
+            listaClassificados[1] = 30343;
+            listaClassificados[2] = 8240;
+            listaClassificados[3] = 3125;
+            listaClassificados[4] = 50525;
+            listaClassificados[5] = 23289;
+            listaClassificados[6] = 7310;
+            listaClassificados[7] = 9281;
+            listaClassificados[8] = 49524;
+            listaClassificados[9] = 33001;
+        }
+
+        private void BtnConsultar1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numInscricao = Convert.ToInt32(TxtNumero.Text);
+
+                for (int x = 0; x <= 9; x++)
+                {
+                    if (listaClassificados[x] == numInscricao)
+                    {
+                        x++;
+                        LblResultado.Text = $"Parabéns!!!" + 
+                            $"Você está classificado" + 
+                            $"{x.ToString()}a. posição!!!";
+                        return;
+                    }
+                }
+                LblResultado.Text = "Lista de espera!!!";
+
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void BtnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numInscricao = Convert.ToInt32(TxtNumero.Text);
+
+                //localiza a posicao do candidato no array se não existir o indice será = -1
+                int posicao = Array.IndexOf(listaClassificados, numInscricao);
+
+                if (posicao < 0)
+                {
+                    LblResultado.Text = "Lista de espera!!";
+
+                }
+
+                else
+                {
+                    posicao += 1;
+                    LblResultado.Text = $"Parabéns!!Você está classificado na" +
+                        $"{posicao.ToString()} posição!!";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Digite somente números!!!", "Atenção");
+                TxtNumero.Text = "";
+                TxtNumero.Focus();
+            }
+            
+
+        }
+    }
+}
+
